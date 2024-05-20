280ebf5f1fa5cbc5d84b7d9c874a1655b1cfd94b
Daniamaher <148376324+Daniamaher@users.noreply.github.com>
2024-05-20 21:58:41 +0300

    Add files via upload

---
+++ Housing/main.dart
@@ -0,0 +1,57 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Housing/pages/Services/FirestoreService.dart';
+import 'package:flutter_application_1/Housing/pages/addhousepage.dart';
+import 'package:flutter_application_1/Housing/pages/housedetailspage.dart';
+import 'package:flutter_application_1/Housing/pages/houseownerloginpage.dart';
+import 'package:flutter_application_1/Housing/pages/HouseOwnerRegistrationPage.dart';
+import 'package:flutter_application_1/Housing/pages/HousesPage.dart';
+
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:flutter_application_1/Housing/pages/Services/FirestoreService.dart';
+import 'package:flutter_application_1/Housing/pages/HousesPage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'House Owner Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: 'houseownerloginpage',
+      routes: {
+          'houseownerloginpage': (context) => HouseOwnerLoginPage(),
+
+        'houseownerregister': (context) => HouseOwnerRegistrationPage (),
+        'houses': (context) => HousingPage(),
+        'addHouse': (context) => AddHousePage(),
+
+        'HouseDetailsPage': (context) => DisplayHouseDetailPage(houseDetails: HouseDetails.defaultData()),
+
+
+
+
+
+      },
+    );
+  }
+
+}
+
+
